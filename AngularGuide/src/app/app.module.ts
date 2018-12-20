cda2755ea3601bc5ece5d93d7c73b5fd232c4ee3
rambabu <babukondaram@gmail.com>
2018-12-20 17:09:44 +0530

    flex

--- AngularGuide/src/app/app.module.ts
+++ AngularGuide/src/app/app.module.ts
@@ -7,13 +7,14 @@ import { MasterComponent } from './modules/master/master.component';
 import { MaterialDesignModule } from './materials.module';
 import { LayoutMaster } from './core/layout/layout-master.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent,
     MasterComponent,
-    LayoutMaster
+    LayoutMaster,
+    FlexLayoutModule
   ],
   imports: [
     BrowserModule,
