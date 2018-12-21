13287fca8cf3a1d037f2ba4c9684d17623fe82fc
rambabu <babukondaram@gmail.com>
2018-12-21 12:04:14 +0530

    layout. design note : npm install

--- AngularGuide/src/app/app.module.ts
+++ AngularGuide/src/app/app.module.ts
@@ -7,14 +7,13 @@ import { MasterComponent } from './modules/master/master.component';
 import { MaterialDesignModule } from './materials.module';
 import { LayoutMaster } from './core/layout/layout-master.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MasterComponent,
-    LayoutMaster,
-    FlexLayoutModule
+    LayoutMaster
   ],
   imports: [
     BrowserModule,
--- AngularGuide/src/app/core/layout/layout-master.component.ts
+++ AngularGuide/src/app/core/layout/layout-master.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, ViewEncapsulation } from "@angular/core";
 import { style } from '@angular/animations';
 
 @Component({
