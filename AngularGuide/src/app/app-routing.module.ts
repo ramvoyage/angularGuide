9a7f4434870c3860af4f5cacfa70b45f43b9c68f
kamal kumar <kamalkumar91613@gmail.com>
2019-01-02 12:24:58 +0530

    login and register component  added
    npm install : no

--- AngularGuide/src/app/app-routing.module.ts
+++ AngularGuide/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path:"Login",component:LoginComponent
+},{path:"Register",component:RegisterComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularGuide/src/app/app.module.ts
+++ AngularGuide/src/app/app.module.ts
@@ -7,18 +7,24 @@ import { MasterComponent } from './modules/master/master.component';
 import { MaterialDesignModule } from './materials.module';
 import { LayoutMaster } from './core/layout/layout-master.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     MasterComponent,
-    LayoutMaster
+    LayoutMaster,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MaterialDesignModule,
+    FormsModule,
     BrowserAnimationsModule
   ],
   providers: [],
