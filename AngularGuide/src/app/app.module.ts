5f713597261b73fb623054410dc70c67b7c42c50
kamal kumar <kamalkumar91613@gmail.com>
2019-01-04 12:00:19 +0530

    reacive module added in app module ts
    npm install : no

--- AngularGuide/src/app/app.module.ts
+++ AngularGuide/src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialDesignModule } from './materials.module';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { DashboardModule } from './core/dashboard/dashboard.module';
 import { LayoutModule } from './core/layout/layout.module';
 import { RegisterComponent } from './register/register.component';
+import { RouteModuleComponent } from './core/route-module/route-module.component';
+import { LoginModule } from './login/login.module';
 
 
 
@@ -23,6 +23,7 @@ import { RegisterComponent } from './register/register.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     MaterialDesignModule,
     AppRoutingModule,
     DashboardModule,
