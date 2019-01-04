f8857011daed5006767f394b7207778b71aa5c5f
kamal kumar <kamalkumar91613@gmail.com>
2019-01-04 11:03:59 +0530

    register

--- AngularGuide/src/app/app.module.ts
+++ AngularGuide/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MasterComponent } from './modules/master/master.component';
 import { MaterialDesignModule } from './materials.module';
@@ -9,7 +10,7 @@ import { LayoutMaster } from './core/layout/layout-master.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { FormsModule } from '@angular/forms';
+
 
 
 @NgModule({
@@ -26,6 +27,8 @@ import { FormsModule } from '@angular/forms';
     MaterialDesignModule,
     FormsModule,
     BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- AngularGuide/src/app/register/register.component.ts
+++ AngularGuide/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 
-import { Component} from "@angular/core";
+import { Component, OnInit} from "@angular/core";
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector:'app-register',
@@ -7,6 +8,24 @@ import { Component} from "@angular/core";
     styleUrls: [`./register.component.css`]
 })
 
-export class RegisterComponent {
+export class RegisterComponent implements OnInit{
     
+    signUpforms:FormGroup;
+ngOnInit(){
+this.signUpforms=new FormGroup({
+    'firstName': new FormControl(null,Validators.required),
+    'lastName': new FormControl(null,Validators.required),
+    'address': new FormControl(null,Validators.required),
+    'email': new FormControl(null,[Validators.required,Validators.email]),
+    'password': new FormControl(null,Validators.required),
+    'gender': new FormControl("male"),
+    'DOB': new FormControl(null,Validators.required),
+})
+}
+onSubmit(){
+    console.log(this.signUpforms);
+}
+submitResister(){
+    alert("Hi Welcome")
+}
 }   
