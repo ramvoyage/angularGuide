eb25de58a79c9e4b4408ee96dce0cf5f4902d362
kamal kumar <kamalkumar91613@gmail.com>
2019-01-07 12:49:39 +0530

    Forms module and reactive forms
    NPM install : No

--- AngularGuide/src/app/login/login.module.ts
+++ AngularGuide/src/app/login/login.module.ts
@@ -3,15 +3,25 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login.routing.module';
 import { LoginComponent } from './login.component';
 import { MaterialDesignModule } from '../materials.module';
+import { RegisterComponent } from '../register/register.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 
 @NgModule({
     imports: [
       CommonModule,
       LoginRoutingModule,
-      MaterialDesignModule
+      MaterialDesignModule,
+      ReactiveFormsModule,
+      FormsModule
     ],
-    declarations: [LoginComponent],
-    exports:[LoginComponent]
+    declarations: [
+      LoginComponent,
+      RegisterComponent
+    ],
+    exports:[
+      LoginComponent,
+      RegisterComponent,
+    ]
   })
   export class LoginModule { }
